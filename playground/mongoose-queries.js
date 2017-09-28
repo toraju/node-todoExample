11102ec2715727bf3e2c0cae298770adf14737cf
Raju Subbanna <hemanth@clouber.io>
2017-09-27 21:07:18 -0700

    update to validate

--- playground/mongoose-queries.js
+++ playground/mongoose-queries.js
@@ -1,8 +1,12 @@
 const {mongoose} = require('./../server/db/mongoose');
 const {Todo}= require('./../server/models/todo');
+const {User}= require('./../server/models/user');
+const {ObjectID} = require('mongodb');
 var id='59c8adcc59085ba5b986abfd9999';
 //var id='59cc07111d089f285658203c';
-
+if(!ObjectID.isValid(id)){
+  console.log('ID not valid');
+}
 Todo.findById(id).then((todo)=>{
   if(!todo){
     return console.log('Id not found');
